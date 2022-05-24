f4f99c4cc3f67cb01d59eeee2dcc4e1816ac0732
mamunur13525 <mdmamunur135255@gmail.com>
2022-05-24 12:27:54 +0600

    footer  update

--- components/Shared/Footer/Footer.js
+++ components/Shared/Footer/Footer.js
@@ -1,52 +1,84 @@
 import React from 'react';
 import style from '../../../styles/Home.module.css';
+import Button from '../Button';
+import { FaPinterestP } from 'react-icons/fa';
+import { IoLogoTwitter, IoLogoInstagram } from 'react-icons/io5';
+import { ImFacebook } from 'react-icons/im';
+
+
+
 
 const Footer = () => {
     return (
         <div style={style} className='relative '>
             <div className='navImage'>
-
             </div>
-            <div className='bg-gray-100'>
-                <div className='container mx-auto py-4 px-10'>
-                    <div className='w-full flex flex-between gap-3'>
-                        <div className='w-full'>
-                            <h1>Title</h1>
-                            <ul className='list-none flex flex-col gap-3'>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
+            <div className="bg-gray-100">
+                <div className="container mx-auto pb-8 pt-12 px-10">
+                    <div className="w-full flex flex-wrap flex-between">
+                        <div className="w-full md:w-1/2 lg:w-3/12">
+                            <h1 className="text-[#80b435] text-xl mb-5">About  Us</h1>
+                            <p className="text-[#2b2b2b] text-[15px]">With more than 15 years of experience we can proudly say that we are one of the best in business, a trusted supplier for more than 1000 companies...</p>
+                            <p className="text-[#80b435] text-xs mt-5">READ MORE </p>
+                        </div>
+                        <div className="w-full md:w-1/2 lg:w-3/12 md:pl-5 lg:pl-5">
+                            <h1 className="text-[#80b435] text-xl mb-5">
+                                Infomation</h1>
+                            <ul className="list-none flex flex-col gap-3 text-gray-600">
+                                <li className="cursor-pointer text-sm">Delivery</li>
+                                <li className="cursor-pointer text-sm">Legal Notice</li>
+                                <li className="cursor-pointer text-sm">Terms &amp; Conditions</li>
+                                <li className="cursor-pointer text-sm">About Us</li>
+                                <li className="cursor-pointer text-sm">Secure Payment</li>
+                                <li className="cursor-pointer text-sm">Our Stores</li>
                             </ul>
                         </div>
-                        <div className='w-full'>
-                            <h1>Title</h1>
-                            <ul className='list-none flex flex-col gap-3'>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
+                        <div className="w-full md:w-1/2 lg:w-3/12">
+                            <h1 className="text-[#80b435] text-xl mb-5">Get It Touch</h1>
+                            <ul className="list-none flex items-center gap-4">
+                                <li className="cursor-pointer"><IoLogoTwitter className='text-xl text-[#80b435]' /></li>
+                                <li className="cursor-pointer"><ImFacebook className='text-xl text-[#80b435]' /></li>
+                                <li className="cursor-pointer"><IoLogoInstagram className='text-xl text-[#80b435]' /></li>
+                                <li className="cursor-pointer"><FaPinterestP className='text-xl text-[#80b435]' /></li>
                             </ul>
+                            <h1 className="mt-10  text-[#80b435] text-xl mb-5">Payment Accept</h1>
+                            <img src="https://cdn.shopify.com/s/files/1/2179/9295/t/5/assets/f1_payment1_img.png?v=169920717840467018591501466879" alt="" />
                         </div>
-                        <div className='w-full'>
-                            <h1>Other Link</h1>
-                            <ul className='list-none flex flex-col gap-3'>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
-                            </ul>
+                        <div className="w-full md:w-1/2 lg:w-3/12 md:pl-5 lg:pl-0">
+                            <h1 className="text-[#80b435] text-xl mb-5">
+                                Get Newsletter</h1>
+                            <p className="text-[#2b2b2b] text-[15px]">
+                                Sed ut perpiciatis  unde omnix  iste  natus error sit voluptatem  accusantium</p>
+                            <div className="w-full my-4">
+                                <input type="email" placeholder="Place enter your email" name="" id="" className="w-full border bg-white py-3 px-2 text-sm text-gray-400 font-light focus:outline-none" />
+                            </div>
+                            <button className=" py-[8px] px-5  shadow hover:bg-green-600 transition-all cursor-pointer bg-[#80b435] text-white">
+                                Subscribe</button>
                         </div>
-                        <div className='w-full'>
-                            <h1>Social Links</h1>
-                            <ul className='list-none flex flex-col gap-3'>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
-                                <li className='cursor-pointer'>#Link</li>
-                            </ul>
-                        </div>
-
+                    </div>
+                </div>
+                <hr />
+                <div className="container py-4 px-10 mx-auto flex flex-wrap justify-between items-center text-sm font-light">
+                    <span className="text-gray-500 mb-3 md:mb-0">
+                        Copyright ©2017 Fresh Food-All Rights  Reserved
+                    </span>
+                    <div className="flex flex-wrap gap-4">
+                        <ul className="flex items-center gap-3 text-gray-500 ">
+                            <li>Contact us</li>
+                            <li>Term of Use Privacy</li>
+                            <li>Policy</li>
+                            <li>Site Map</li>
+                        </ul>
+                        <span className="hidden  md:block w-1 mx-3 border-r border-gray-400">
+                        </span>
+                        <ul className="flex items-center gap-3 text-gray-500">
+                            <li>Language: <span className="text-[#80b435] font-medium">
+                                ENG</span>
+                            </li>
+                            <li>Price: <span className="text-[#80b435] font-medium">
+                                USD</span>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -56,3 +88,5 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
