41b6d15b06bb5cd768e4a14896373200972b8737
Mamun React W3Dev <mamun@w3dev.email>
2022-06-17 02:58:01 +0600

    sidebar cart update

--- components/Shared/Cart/CartSidebar.js
+++ components/Shared/Cart/CartSidebar.js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { GrClose } from 'react-icons/gr';
+import { RiDeleteBin6Line } from 'react-icons/ri';
 import Button from '../Button'
 
 const CartSidebar = ({ cart }) => {
@@ -18,30 +19,25 @@ const CartSidebar = ({ cart }) => {
     }
     return (
         <div className={`${cartShow} transition-transform duration-500 shadow-xl border-l fixed right-0 top-0  h-full z-[125] w-96  pt-0 bg-white`}>
-            <div className='relative bg-gray-100 py-6 text-center'>
+            <div className='relative  py-6 text-center'>
                 <GrClose onClick={() => setShowCart(false)} className='absolute left-3 top-3 hover:animate-pulse  cursor-pointer rounded-full text-xl' />
                 <p className='text-2xl font-light'>Shopping Cart</p>
             </div>
             <div className=' h-[70%] overflow-auto  pl-2 pr-4'>
                 {
-                    Array.isArray(cartItems) && cartItems.map(item => (
-                        <div key={item} className='flex justify-between items-center py-3 border-b border-gray-100'>
-                            <div className='flex gap-2'>
-                                <img className='w-[80px]' src="https://cdn.shopify.com/s/files/1/2179/9295/products/1_small.jpg?v=1500458217" alt="" />
-                                <div className='flex flex-col'>
-                                    <p className='text-xl uppercase font-medium'>title</p>
-                                    <p className='text-xl text-[#4f8700] font-medium'>$300.00</p>
-                                    <p className='text-[15px] uppercase'>Quantity: 1</p>
-                                </div>
-                            </div>
-                            <GrClose onClick={() => removeCartItem(item)} className='  hover:animate-pulse  cursor-pointer rounded-full' />
+                    Array.isArray(cartItems) &&
+                        cartItems.length ?
+                        cartItems.map(item => (
+                            <CartItem removeCartItem={removeCartItem} key={item} item={item} />
+                        )) :
+                        <div className='h-full grid place-items-center'>
+                            <img src="https://shop.unicornstore.in/beam/themes/2019/assets/img/cart_empty.png" alt="" />
                         </div>
-                    ))
                 }
 
             </div>
 
-            <div className='bg-gray-100 absolute bottom-0 p-5 w-full right-0'>
+            <div className=' absolute bottom-0 p-5 w-full right-0'>
                 <div className='flex justify-between items-center py-4 px-2  uppercase'>
                     <p className='font-medium text-lg'>Total Price</p>
                     <p className='font-medium text-xl  text-[#4f8700]'>$300.00</p>
@@ -60,3 +56,39 @@ const CartSidebar = ({ cart }) => {
 };
 
 export default CartSidebar;
+
+
+const CartItem = ({ removeCartItem, item }) => {
+    const [quantity, setQuantity] = useState(1);
+
+    const increaseQuantity = () => {
+        setQuantity(prev => prev + 1)
+    }
+    const decreaseQuantity = () => {
+        if (quantity > 1) setQuantity(prev => prev - 1)
+    }
+    return (
+        <div className='flex justify-between  shadow bg-gray-50 p-3 py-2 mb-3 rounded-md'>
+            <div className='flex gap-2'>
+                <img className='w-[80px] rounded-md' src="https://cdn.shopify.com/s/files/1/2179/9295/products/1_small.jpg?v=1500458217" alt="" />
+                <div className='flex flex-col'>
+                    <p className='text-console.log(); uppercase font-medium text-green-700'>title</p>
+                    <p className='text-gray-500 text-sm'>this is description</p>
+                    <div className='flex  gap-2 mt-2'>
+                        <div onClick={decreaseQuantity} className='border h-6 w-6 rounded text-xl flex items-center justify-center cursor-pointer shadow hover:bg-gray-100'>
+                            <span className='pb-1'>-</span>
+                        </div>
+                        <p className='text-[15px] uppercase'>{quantity}</p>
+                        <div onClick={increaseQuantity} className='border h-6 w-6 rounded text-xl flex items-center justify-center cursor-pointer shadow hover:bg-gray-100'>
+                            <span className='pb-1'>+</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='flex h-[5rem]   flex-col justify-between items-end'>
+                <RiDeleteBin6Line onClick={() => removeCartItem(item)} className='  hover:animate-pulse  text-gray-600 cursor-pointer rounded-full' />
+                <p className='text-xl text-[#4f8700] font-medium'>$300.00</p>
+            </div>
+        </div>
+    )
+}
